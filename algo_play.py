ee73adff33e2141de88c86cd0f520833f9e05dfc
unknown <lourou99@gmail.com>
2022-10-18 23:50:12 +0200

    update functions

--- algo_play.py
+++ algo_play.py
@@ -18,7 +18,7 @@ class Algo_play():
         self.valr = Valr()
         self.SETTINGS = Settings()
 
-        self.TRADEPAIR_ALLOWED =  ["ETH", "BTC", "XRPZ", "BNB", "SOL", "AVAX", "SHIB"]
+        self.TRADEPAIR_ALLOWED =  ["XRP", "BNB", "SOL", "AVAX", "SHIB"]
 
         # Compatible trading pairs.
         self.VALR_COINPAIR = ["ETHZAR", "BTCZAR", "XRPZAR", "BNBZAR", "SOLZAR", "AVAXZAR", "SHIBZAR"]
@@ -32,8 +32,8 @@ class Algo_play():
     def run(self):
         """ Main trading algo happens here."""
 
-        cr = CurrencyRates()
-        self.ZAR = cr.get_rates("USD")["ZAR"]
+        # cr = CurrencyRates()
+        # self.ZAR = cr.get_rates("USD")["ZAR"]
         self.valr_data = self.valr.return_coinPair_group(coinpair_group=self.VALR_COINPAIR)
         kucoin_data = self.kucoin.return_coinPair_group(coinpair_group=self.KUCOIN_COINPAIR)
         
@@ -45,7 +45,7 @@ class Algo_play():
 
 
 
-    def execute_trade(self, coin):
+    def execute_trade(self, coin, percent_difference, percent_increase):
         """ Run the execution of the trade between the exchanges. """
 
         enough_usdc = None
@@ -57,38 +57,46 @@ class Algo_play():
         # 0. check if coin has already been bought.
         coin_account = self.kucoin.get_account(coin=coin, accounts=all_accounts)
         if coin_account != None:
-            if float(coin_account["balance"]) >= 50:
+            if float(coin_account["balance"]) >= 10000000: # NEED TO FIX THIS, IT CURRENTLY IS BASED ON BASE COIN CURRENCY AND NOT USDT
+                print("enough coins")
                 self.coin_account_value = True
 
         # 1. check if enough USDC in account.
-        USDC_account = self.kucoin.get_account(coin="USDC", accounts=all_accounts)
-        if USDC_account != None:
-            if float(USDC_account["balance"]) >= 50:
+        USDT_account = self.kucoin.get_account(coin="USDT", accounts=all_accounts)
+        USDT_balance = USDT_account["balance"]
+        if USDT_account != None:
+            if float(USDT_balance) >= 40:
                 enough_usdc = True
+
+        coin_pairs = coin + '-USDT'
+        print("\n*** Arbitrage oppertunity - ", coin, str(percent_difference) + "%", " | R"+ str(percent_increase), " | enough USDC: " + str(bool(enough_usdc)), " | Existing coins: " + str(bool(self.coin_account_value)))
 
 
         # 2. execute a buy market order on international exchange,
         if self.coin_account_value != None:
             # 2.1. execute transfer to local wallet.
-            Kucoin.transfer_coin_to_address(coin=coin, amount=10, address="1234")
+            self.kucoin.transfer_coin_to_address(coin=coin, amount=10, address="1234")
 
 
         elif self.coin_account_value == None:
             # 2.1. BUY WITH USDC, if no USDC available, the pass
             if enough_usdc == True:
-                Kucoin.buy_coin(coin, 50)
+                self.kucoin.buy_coin(coin_pair=coin_pairs, amount_in_USDT=float(USDT_balance) - 1)
 
                 # 2.2. while loop until funds reflect in the coins address,
                 KUCOIN_Funds_ready = self.wait_for_funds_KUCOIN(coin)
 
                 # 2.3. execute transfer to local wallet.
                 if KUCOIN_Funds_ready == True:
-                    Kucoin.transfer_coin_to_address(coin=coin, amount=10, address="1234")
+                    self.kucoin.transfer_coin_to_address(coin=coin, amount=10, address="1234")
 
 
 
         # 3. Check if crypto has arrived on local wallet.
-        VALR_Funds_readyself = self.wait_for_funds_VALR(coin)
+        if self.coin_account_value != None:
+            VALR_Funds_readyself = self.wait_for_funds_VALR(coin)
+        else:
+            VALR_Funds_readyself = False
 
         # 4. If True - execute a sell market order on local exchange
         if VALR_Funds_readyself == True:
@@ -116,11 +124,12 @@ class Algo_play():
             bal = self.valr.get_balances(acc_ID=ID)
             local_coin_wallet = self.valr.parse_balances(balances=bal, coin=coins)
 
-            print("Checking Funds. - ", local_coin_wallet, coins)
+            print("VALR - Checking Funds. - ", local_coin_wallet, coins)
 
             if float(local_coin_wallet) >= float(50):
                 self.coin_account_value = True
                 if self.coin_account_value == True:
+                    print("VALR - Funds True", coins)
                     break
 
         return True
@@ -137,11 +146,12 @@ class Algo_play():
             # Get all Kucoin Accounts
             all_accounts = self.kucoin.client.get_accounts()
             coin_account = self.kucoin.get_account(coin=coin, accounts=all_accounts)
-            print("Checking Funds. - ", coin_account["balance"], coin)
+            print("kucoin - Checking Funds. - ", coin_account, coin)
             if coin_account != None:
                 if float(coin_account["balance"]) >= 50:
                     self.coin_account_value = True
                     if self.coin_account_value == True:
+                        print("KUCOIN - Funds True", coin)
                         break
 
         return True
@@ -151,6 +161,8 @@ class Algo_play():
     def signal(self, return_analyse):
         """ Runs through the analyse data and if data is true, execute a True signal. """
 
+        percent_trigger = self.SETTINGS.percent_trigger # the value that will trigger as an oppertunity, 4% price increase would trigger an abritrage for example.
+
         for data in return_analyse:
             for allowed in self.TRADEPAIR_ALLOWED:
                 if allowed in data:
@@ -159,8 +171,8 @@ class Algo_play():
                     growth_potential = return_analyse[data][1]            
 
                     # if the arbitrage spread is greater than. then execute the trade.
-                    if percent_difference >= 2:
-                        self.execute_trade(coin=allowed)
+                    if percent_difference >= percent_trigger:
+                        self.execute_trade(allowed, percent_difference, growth_potential)
 
 
 
@@ -201,9 +213,7 @@ class Algo_play():
         percent_difference = round(utils.get_percentage_difference(loc_bid, int_coin_zar), 2)
         percent_increase = round(utils.percent_increase(percent_difference, self.SETTINGS.play_money), 3)
 
-        print(loc_currencyPair, " |  R "+str(percent_increase), " | "+str(percent_difference) + "%", " | Valr price R"+ str(loc_bid), " | Kucoin price R"+str(round(float(int_coin_zar), 2)))
-        if percent_difference >= 3:
-            print(loc_currencyPair, " |  R "+str(percent_increase), " | "+str(percent_difference) + "%", "<<< **************** ")
+        self.print_statement(loc_bid, int_coin_zar, loc_currencyPair, percent_difference, percent_increase)
 
         return percent_difference, percent_increase
 
@@ -211,11 +221,11 @@ class Algo_play():
 
 
 
-    def print_statement(self, loc_bid, int_ask, loc_currencyPair, percent_difference, percent_increase):
+    def print_statement(self, loc_bid, int_coin_zar, loc_currencyPair, percent_difference, percent_increase):
         """ Prints out. """
 
         if self.SETTINGS.print_statements == True:
-            print(loc_currencyPair, " |  R "+str(percent_increase), " | "+str(percent_difference) + "%", " | locZAR R"+ str(self.valr_data[1]["bidPrice"]), " | IntZAR R"+str(round(self.ZAR, 2)))
+            print(loc_currencyPair, " |  R "+str(percent_increase), " | "+str(percent_difference) + "%", " | Valr price R"+ str(loc_bid), " | Kucoin price R"+str(round(float(int_coin_zar), 2)))
             if percent_difference >= 3:
                 print(loc_currencyPair, " |  R "+str(percent_increase), " | "+str(percent_difference) + "%", "<<< **************** ")
 
