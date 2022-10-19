a9a7f72be2d47558fbc06878c5704133c9d25a9c
unknown <lourou99@gmail.com>
2022-10-19 23:37:53 +0200

    .

--- settings.py
+++ settings.py
@@ -8,12 +8,15 @@ class Settings():
 
         # General
         self.Title = "ArbPlay"
-        self.play_money = 100000
+        self.play_money = 10000
+        self.force_signal = False, "XRP"     # Run tests on specific coin.
         self.print_statements = True
 
         # Kucoin
-        self.execute_withdrawels = False # if True, withdraw / transfer coin to another wallet address.
-        self.execute_order = False # If True, then buy a coin using usdc
+        self.trade_account = "trade"        # use either "trade" or "main" accounts
+        self.execute_withdrawels = False    # if True, withdraw / transfer coin to another wallet address.
+        self.execute_order = False          # If True, then buy a coin using usdc
+        self.execute_transfer = True
 
 
         # Valr
@@ -22,4 +25,4 @@ class Settings():
 
 
         # Prices valuation
-        self.percent_trigger = 1.5
+        self.percent_trigger = 1.8
