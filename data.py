fd52234cd7adada4bd11c72c7753a2b0c75e894d
unknown <lourou99@gmail.com>
2022-10-20 23:36:08 +0200

    class for data movement

---
+++ data.py
@@ -0,0 +1,83 @@
+import os
+import utils as utils
+from settings import Settings
+
+
+class Data_log():
+    """ A class for data movement. """
+
+    def __init__(self):
+        """ Initialize data settings. """
+
+        self.SETTINGS = Settings()
+
+        self.data_path = self.return_arbitrage_trade()
+        self.data_file = utils.read_json(self.data_path) 
+
+
+
+    
+    def return_arbitrage_trade(self):
+        """ returns the path to the arbitrage json file. """
+
+        this_file = os.path.dirname(__file__)
+        data_file = f"{this_file}/data/arbitrage.json"
+
+        if os.path.exists(data_file) != True:
+            data = {}
+            utils.write_to_json(data_file, data)
+
+        return data_file
+
+
+
+
+    def return_funds_position(self):
+        """ Returns the funds position. """
+
+        position = utils.read_json(self.data_path)["position"]
+        return position
+
+
+
+
+    def set_kucoin_USDT(self, USDT):
+        """ Sets the current USDT balance before execution. """
+
+        self.data_file["Kucoin_USDT"] = USDT
+        utils.write_to_json(self.data_path, self.data_file)  
+
+
+
+
+    def set_kucoin_Coin(self, coin):
+        """ Sets the current USDT balance before execution. """
+
+        self.data_file["Kucoin_coin"] = coin
+        utils.write_to_json(self.data_path, self.data_file)  
+
+
+
+
+    def set_kucoin_amount(self, amount_coins):
+        """ Sets the current USDT balance before execution. """
+
+        self.data_file["Kucoin_amount"] = amount_coins
+        utils.write_to_json(self.data_path, self.data_file)  
+
+
+
+
+    def set_fund_position(self, position):
+        """ sets the position of the funds. """
+
+        self.data_file["position"] = position
+        utils.write_to_json(self.data_path, self.data_file)
+
+
+
+
+
+if __name__ == "__main__":
+
+    data_log = Data_log()
