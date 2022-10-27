479f3a163344f0c2ae8e684bf8d0722d906b6b6b
unknown <lourou99@gmail.com>
2022-10-27 21:52:46 +0200

    building dripdrip

--- DripDrip/dripdrip.py
+++ DripDrip/dripdrip.py
@@ -17,7 +17,7 @@ class DripDrip():
 
         VALR_EXCHANGE = VALR.Valr()
 
-        self.days = 21
+        self.days = 10
         self.amount_capital = 10000
         self.sampleList = ["XRP", "BTC", "ADA", "SOL"]
         self.weights = [70, 50, 20, 10]
@@ -35,9 +35,39 @@ class DripDrip():
         """ Sets the drip plan into the data.json. """
         pass
 
+
+
+
     def calculate_plan(self):
-        """ calculates returns values for each coin. """
-        pass
+        """ calculates the amount it must invest for each coin over the amount of days, 
+            and sets it in the data. 
+        """
+        
+        my_data = self.data
+
+        coins = []
+        coin_values = []
+
+        for data in my_data:
+            value = my_data[data]
+
+            coins.append(data)
+            coin_values.append(value)
+
+        randomList = random.choices(coins, weights=coin_values, k=self.amount_capital)
+            
+        data = {}
+        for i in coins:
+
+            coin_amount = randomList.count(i)
+            data[i] = coin_amount / self.days
+
+        print(data)
+
+
+
+
+
 
     def check_newDay(self):
         """ Check if it is a new day, if True, execute trade. """
@@ -57,43 +87,56 @@ class DripDrip():
             # Collect data
             drip_data = utils.read_json(f"{os.path.dirname(__file__)}/drip_data.json")
 
-            randomList = random.choices(self.sampleList, weights=self.weights, k=self.amount_capital)
-            
-            xrp_count = randomList.count("XRP")
-            btc_count = randomList.count("BTC")
-            ada_count = randomList.count("ADA")
-            sol_count = randomList.count("SOL")
-
-            days_amount = xrp_count / self.days, btc_count / self.days, ada_count / self.days, sol_count / self.days
-
-            xrp = days_amount[0]
-            btc = days_amount[1]
-            ada = days_amount[2]
-            sol = days_amount[3]
-
-            x = 0
-            b = 0
-            a = 0
-            s = 0
-
-            print("Run --", days_amount, "\n")
-
-            for i in range(1, self.days + 1):
-                
-                x += xrp
-                b += btc
-                a += ada
-                s += sol
-
-                print("Day:",i , round(x,2), round(b,2), round(a,2), round(s,2))
+            # If time is now, then invest
+            if drip_data["time"] == self.invest_time:
+                pass
 
 
-            print("\nTotal -----", x, b, a, s)
-            print("=", x + b + a + s)
+                if drip_data["already_invested"] != True:
+                    pass
+
+
+
 
 
 
 if __name__ == "__main__":
 
     DRIP = DripDrip()
-    DRIP.run()    
+    DRIP.calculate_plan()   
+
+
+
+    # randomList = random.choices(self.sampleList, weights=self.weights, k=self.amount_capital)
+    
+    # xrp_count = randomList.count("XRP")
+    # btc_count = randomList.count("BTC")
+    # ada_count = randomList.count("ADA")
+    # sol_count = randomList.count("SOL")
+
+    # days_amount = xrp_count / self.days, btc_count / self.days, ada_count / self.days, sol_count / self.days
+
+    # xrp = days_amount[0]
+    # btc = days_amount[1]
+    # ada = days_amount[2]
+    # sol = days_amount[3]
+
+    # x = 0
+    # b = 0
+    # a = 0
+    # s = 0
+
+    # print("Run --", days_amount, "\n")
+
+    # for i in range(1, self.days + 1):
+        
+    #     x += xrp
+    #     b += btc
+    #     a += ada
+    #     s += sol
+
+    #     print("Day:",i , round(x,2), round(b,2), round(a,2), round(s,2))
+
+
+    # print("\nTotal -----", x, b, a, s)
+    # print("=", x + b + a + s)
